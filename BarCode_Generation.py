f39ba35b58a049f4e497cb148ea93b8e85750e32
mohammednasirhussain <69389975+mohammednasirhussain@users.noreply.github.com>
2025-12-24 19:53:57 +0530

    Update BarCode_Generation.py

--- BarCode_Generation.py
+++ BarCode_Generation.py
@@ -1,7 +1,10 @@
+#pip install python-barcode pillow
+
 #Barcode generation
 
 from barcode import EAN13
 from barcode.writer import ImageWriter
 
 code = EAN13("5901234123457", writer = ImageWriter())
+
 filename = code.save("ean13_barcode")
