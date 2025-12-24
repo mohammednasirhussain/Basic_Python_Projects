512c8ff8c1b84f086af37d1ec30689e72ae93e1c
mohammednasirhussain <69389975+mohammednasirhussain@users.noreply.github.com>
2025-12-24 19:57:56 +0530

    Update BarCode_Generation.py

--- BarCode_Generation.py
+++ BarCode_Generation.py
@@ -8,3 +8,15 @@ from barcode.writer import ImageWriter
 code = EAN13("5901234123457", writer = ImageWriter())
 
 filename = code.save("ean13_barcode")
+
+
+
+#Method 2
+import barcode
+from barcode.writer import ImageWriter
+
+def generate_barcode(data, filename):
+    code = barcode.get("code128", data, writer=ImageWriter())
+    code.save(filename)
+
+generate_barcode("1234567890", "barcode")
